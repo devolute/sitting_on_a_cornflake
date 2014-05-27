9abefeccc46e3d7bda47fca067e3680db1014f51
Tommaso Romito <tommaso@devolute.org>
2014-05-27 10:22:55 +0200

    retting up rails and git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SittingOnACornflake::Application.config.secret_key_base = '799728af209da5c061ee253ad27520f8fbd139e549fbb31b9a1afc47647ebbad7f7b83534064437f65efa55925c50c667f67c3be54588e559bf36793ce637899'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SittingOnACornflake::Application.config.session_store :cookie_store, key: '_sitting_on_a_cornflake_session'
